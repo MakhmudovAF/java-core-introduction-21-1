1456c181107e1da6b31ecb404ee72d0fb5e2faba
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-11-12 17:54:49 +0300

    feat: добавить класс Practicum

---
+++ src/Practicum.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        List<MountainHare> hares = List.of(
+                new MountainHare(4, 4.4, 120),
+                new MountainHare(7, 3.6, 150),
+                new MountainHare(1, 2.3, 100)
+        );
+
+        System.out.println("В лесу лето!");
+        Forest forest = new Forest(hares, "лето");
+
+        System.out.println("Список зайцев:");
+        forest.printHares();
+
+        System.out.println("В лесу зима!");
+        Forest.setSeason("зима");
+
+        System.out.println("Список зайцев:");
+        forest.printHares();
+    }
+}
